3fb5ac46e78c1b241159dfe69f364b4557c77c19
zanyihou <419015894@qq.com>
2017-08-26 17:48:14 +0800

    v3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ server = require('http').createServer(app);
 io = require('socket.io').listen(server);
 
 app.use('/',express.static(__dirname + '/'));
-server.listen(8081);
+server.listen(80);
 var users = [];
 var usernum = 0;
 
